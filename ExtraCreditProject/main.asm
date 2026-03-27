dece5029d5cc79825f4a565b475cf3281d37e6fb
markp561 <mpepaj1@gmail.com>
2026-03-27 19:58:29 -0400

    Fixed bugs in the binary search function in ExtraCreditProject/main.asm

--- ExtraCreditProject/main.asm
+++ ExtraCreditProject/main.asm
@@ -5,15 +5,14 @@ default rel
 section     .data
     fmt         db "%d", 10, 0
 
-    arr         dq 2, 1, 5, 4, 3
+    arr         dq 20, 10, 50, 40, 30
     arr_len     equ ($ - arr) / 8
 
     q           dq 0
     i           dq 0
     j           dq 0
         
-    target      dq 1
-    mid         dq 0
+    target      dq 20
     
     
 section     .text
@@ -36,13 +35,14 @@ main:
     call print_array
 
 
-    mov rdi, arr
-    mov rsi, 0
-    mov rdx, arr_len
+    mov rdi, arr        ; move base address of arr into rdi as first argument of binary search
+    mov rsi, 0          ; move 0 into rsi as start index of arr as second argument of binary search
+    mov rdx, arr_len    ; move the length of arr into rdx as third argument of binary search
     dec rdx
+    mov rcx, 0          ; move 0 into rcx as mid index of arr as fourth argument of binary search
+    mov r8, [target]    ; move the search target into r8 as the fifth argument of binary search
     call binary_search
 
-    
     mov rdi, fmt
     mov rsi, rax
     xor rax, rax
@@ -81,17 +81,17 @@ print_array_end:
 
 partition:
 
-    push rsi            ; push current value of rsi (start) onto stack
-    dec rsi             ; decrement rsi (start - 1)
-    mov [i], rsi        ; move start - 1 to i
-    pop rsi             ; pop rsi to get (start) back
+    push rsi                    ; push current value of rsi (start) onto stack
+    dec rsi                     ; decrement rsi (start - 1)
+    mov [i], rsi                ; move start - 1 to i
+    pop rsi                     ; pop rsi to get (start) back
     
 
-    mov [j], rsi        ; move start into j
+    mov [j], rsi                ; move start into j
     mov rax, [j]
-    cmp rax, rdx        ; compare j to end
-    jl loop             ; if j smaller than end enter loop
-    jge partition_end   ; else go to end of loop
+    cmp rax, rdx                ; compare j to end
+    jl loop                     ; if j smaller than end enter loop
+    jge partition_end           ; else go to end of loop
    
     loop:   
         mov rcx, [j]            ; move j into rcx
@@ -178,50 +178,43 @@ base_case:             ; in the base case we just need to exit the function
 
 binary_search:
     
-    cmp rsi, rdx                ; Compare low index to high index
-    jg binary_search_end        ; If low is greater than high, then exit by jumping to binary_search_end
+    begin_loop:
+        cmp rsi, rdx                ; Compare low index to high index
+        jg .not_found               ; If low is greater than high, then exit by jumping to binary_search_end
 
 
-    mov rax, rdx                ; Move high index to eax
-    add rax, rsi                ; Add low to high
-    cdq                         ; Extend eax to edx:eax to prepare for division
+        mov rax, rsi                ; move low index to rax
+        add rax, rdx                ; add high to low
+        shr rax, 1                  ; shift right by 1 bit to divide by 2
+        mov rcx, rax                ; mov the quotient into rcx
 
-    mov rcx, 2                  ; Move constant value 2 into ecx
-    idiv rcx                    ; Divide by 2
-    mov [mid], rax              ; Move the quotient into mid index
+        mov rax, [rdi + rcx*8]      ; Move element at index mid into eax
 
+                                    ; The following three comparisons are made:
+        cmp r8, rax           
+        jg .greater_than            ; target > arr[mid]                
+        jl .less_than               ; target < arr[mid]
+        je .found                   ; target == arr[mid]
 
-    mov rcx, [mid]              ; Move mid index into ecx
-    mov rax, [rdi + rcx*4]      ; Move element at index mid into eax
+    .greater_than:
+                                    ; low = mid + 1
+        mov rsi, rcx
+        inc rsi
+        jmp begin_loop              ; Jump to the beginning of the binary_search section
 
-                                ; The following three comparisons are made:
-    cmp [target], rax           
-    jg greater_than             ; target > arr[mid]                
-    jl less_than                ; target < arr[mid]
-    je found                    ; target == arr[mid]
-
-greater_than:
-                            ; low = mid + 1
-    inc dword [mid]         ; Increment mid by 1
-    mov rax, [mid]          ; Move mid into eax
-    mov rsi, rax          ; Move value stored in eax into low
-    jmp binary_search ; Jump to the beginning of the binary_search section
-
-less_than:
-                            ; high = mid - 1
-    dec dword [mid]         ; Decrement mid by 1
-    mov rax, [mid]          ; Move mid into eax
-    mov rdx, rax         ; Move value stored in eax into high
-    jmp binary_search       ; Jump to the beginning of the binary_search section
+    .less_than:
+                                    ; high = mid - 1
+        mov rdx, rcx               
+        dec rdx                     
+        jmp begin_loop              ; Jump to the beginning of the binary_search section
 
 
-found:
-                            ; target found
-    mov rax, [mid]          ; Move mid into eax
-
-    jmp binary_search_end   ; target was found so we can exit the function and return the index where it was found
-    
-binary_search_end:
-    ret        
+    .found:
+                                    ; target found
+        mov rax, rcx
+        ret                         ; target was found so we can exit the function and return the index where it was found
+    .not_found:
+        mov rax, -1
+        ret
 
 
